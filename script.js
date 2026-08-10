b980a6c5895a5f0e2a960685f8cf725afdf0b8ea
CreepetPer <creepetper@gmail.com>
2026-08-11 00:15:33 +0800

    fix: update the logic of the digit and operator buttons to ensure that num2 is stored correctly and only runs when required

--- script.js
+++ script.js
@@ -2,19 +2,39 @@ let num1 = '';
 let operator = '';
 let num2 = '';
 
+const validOperations = ['+', '-', '×', '÷'];
+let lastClicked = null;
+document.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        lastClicked = event.target.textContent;
+        console.log('Last button clicked:', lastClicked);
+    }
+});
+
 let digit = document.querySelectorAll(".digit");
+let currentOperator = document.querySelectorAll(".operator");
 let display = document.querySelector("#display");
 
 digit.forEach(element => {
     element.addEventListener("click", function () {
-        num1 = String(num1) + String(element.textContent);
-        display.textContent = num1;
+        if (operator === '') {
+            num1 = String(num1) + String(element.textContent);
+            display.append(num1);
+        } else {
+            num2 = String(num2) + String(element.textContent);
+            display.append(num2);
+        }
     });
 });
-
-function btnDigitFunc(e) {
-    num1 = digit.textContent;
-};
+// TODO: remove '0' when first digit is clicked
+currentOperator.forEach(element => {
+    element.addEventListener("click", function () {
+        if (!validOperations.includes(lastClicked)) {
+            operator = String(element.textContent);
+            display.append(operator);
+        }
+    });
+});
 
 // calculator operation functions
 function add(num1, num2) {
@@ -70,8 +90,8 @@ function input() {
 }
 
 /* 
-1. when btn is activated, it stores that digit to num1 var
-2. when operator btn is activated, it stores that to operator var
+(done) 1. when btn is activated, it stores that digit to num1 var
+(done) 2. when operator btn is activated, it stores that to operator var
 3. when btn is activated again, it stores that digit to num2 var
 if operator is activated a second time, solution = operate becomes num1
 4. when equal button is activated, operate() takes num1, operator, and num2 as its arguments, and run the function and save value to solution var
