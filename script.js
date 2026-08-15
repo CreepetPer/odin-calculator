932eaed7f7a1fe7f2070d03afb1861f6cf18d875
CreepetPer <creepetper@gmail.com>
2026-08-15 20:30:03 +0800

    fix: enhance operator handling and result calculation logic for improved accuracy

--- script.js
+++ script.js
@@ -7,26 +7,13 @@ display.textContent = '0';
 let num1 = '';
 let operator = '';
 let num2 = '';
+let result = '';
 
 const validOperations = ['+', '-', '×', '÷'];
 // track last clicked
 let lastClicked = null;
-document.addEventListener('click', (event) => {
-    if (event.target.tagName === 'BUTTON') {
-        lastClicked = event.target.textContent;
-        console.log('Last button clicked:', lastClicked);
-    }
-});
-
-// track operator click count
+// operator click count
 let opClickCount = 0;
-currentOperator.forEach(element => {
-    element.addEventListener('click', (event) => {
-        if (!validOperations.includes(lastClicked)) {
-            opClickCount++;
-        }
-    });
-});
 
 digit.forEach(element => {
     element.addEventListener("click", function () {
@@ -41,35 +28,54 @@ digit.forEach(element => {
             num2 = String(num2) + String(element.textContent);
             display.append(element.textContent);
         }
+        lastClicked = element.textContent;
     });
 });
-
+// TODO: be able to change operator while last clicked is operator
+// TODO: make AC button functional
 currentOperator.forEach(element => {
     element.addEventListener("click", function () {
+        // track operator click count
+        if (!validOperations.includes(lastClicked)) {
+            opClickCount++;
+            console.log('opClickCount:', opClickCount);
+
+        }
+
+        if (isNaN(num1)) {
+            return;
+        }
+
         if (!validOperations.includes(lastClicked)) {
             if (opClickCount < 2) {
                 operator = String(element.textContent);
             }
-            display.append(operator);
             if (opClickCount >= 2) {
-                num1 = operate(operator, +num1, +num2);
+                result = operate(operator, +num1, +num2);
+                num1 = result;
+                console.log('equals:', result);
                 num2 = '';
-                console.log('equals:', operate(operator, +num1, +num2));
+            }
+            if (result === 'Cannot divide by zero') {
+                display.textContent = 'Cannot divide by zero';
+                return;
             }
 
             operator = String(element.textContent);
             display.textContent = `${num1 + operator}`;
+            lastClicked = element.textContent;
         }
     });
 });
 
 equals.addEventListener("click", function () {
+    result = operate(operator, +num1, +num2);
     display.textContent = '';
-    display.append(operate(operator, +num1, +num2));
-    console.log('equals:', operate(operator, +num1, +num2));
+    display.append(result);
+    console.log('equals:', result);
 
     if (opClickCount >= 1) {
-        num1 = operate(operator, +num1, +num2);
+        num1 = result;
         num2 = '';
     }
     opClickCount = 0;
@@ -106,24 +112,24 @@ function factorial(value) {
 
 // const solution = operate(operator, num1, num2);
 
-function operate(operator, num1, num2) {
+function operate(operator, number1, number2) {
     if (operator === '+') {
-        return add(num1, num2);
+        return add(number1, number2);
     } else if (operator === '-') {
-        return subtract(num1, num2);
+        return subtract(number1, number2);
     } else if (operator === '×') {
-        return multiply(num1, num2);
+        return multiply(number1, number2);
     } else if (operator === '÷') {
-        if (num2 === 0) {
+        if (number2 == 0 && num2 !== '') {
             document.getElementById("display").style.fontSize = "1.9rem";
             return 'Cannot divide by zero';
         } else {
-            return divide(num1, num2);
+            return divide(number1, number2);
         }
     } else if (operator === '^') {
-        return power(num1, num2);
+        return power(number1, number2);
     } else if (operator === '!') {
-        return subtract(num1, num2);
+        return subtract(number1, number2);
     } else {
         return "unsupported operator";
     }
